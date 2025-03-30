02156e81bdaffc57012e9125c391ca778a1a6299
Rhys Koedijk <rhys@koedijk.co.nz>
2025-03-30 13:21:38 +1300

    Reset grid spacing to default

--- SCMM.Web.Client/Program.cs
+++ SCMM.Web.Client/Program.cs
@@ -94,6 +94,8 @@ public static class WebAssemblyHostExtensions
             config.SnackbarConfiguration.RequireInteraction = true;
             config.SnackbarConfiguration.PreventDuplicates = true;
             config.SnackbarConfiguration.ClearAfterNavigation = false;
+
+            MudGlobal.GridDefaults.Spacing = 4;
         });
 
         services.AddSyncfusionBlazor();
