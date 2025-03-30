06bdbff4a0dd6fed897e6f69a82afaff45210486
Rhys Koedijk <rhys@koedijk.co.nz>
2025-03-30 14:03:10 +1300

    Restore support for responsive dialogs

---
+++ SCMM.Web.Client/Shared/Dialogs/ResponsiveDialog.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using MudBlazor.Services;
+
+namespace SCMM.Web.Client.Shared.Dialogs;
+
+public abstract class ResponsiveDialog : ComponentBase, IBrowserViewportObserver, IAsyncDisposable
+{
+    [Inject]
+    private IBrowserViewportService BrowserViewportService { get; set; }
+
+    [CascadingParameter]
+    private IMudDialogInstance Dialog { get; set; }
+
+    protected abstract ResponsiveDialogOptions DialogOptions { get; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        await Dialog.SetOptionsAsync(DialogOptions);
+        await BrowserViewportService.SubscribeAsync(this, fireImmediately: true);
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await BrowserViewportService.UnsubscribeAsync(this);
+    }
+
+    Guid IBrowserViewportObserver.Id { get; } = Guid.NewGuid();
+
+    ResizeOptions IBrowserViewportObserver.ResizeOptions { get; } = new()
+    {
+        ReportRate = 250,
+        NotifyOnBreakpointOnly = true
+    };
+
+    async Task IBrowserViewportObserver.NotifyBrowserViewportChangeAsync(BrowserViewportEventArgs browserViewportEventArgs)
+    {
+        if (await BrowserViewportService.IsBreakpointWithinReferenceSizeAsync(DialogOptions.FullscreenBreakpoint, browserViewportEventArgs.Breakpoint))
+        {
+            await Dialog.SetOptionsAsync(Dialog.Options with
+            {
+                FullScreen = true
+            });
+        }
+        else
+        {
+            await Dialog.SetOptionsAsync(Dialog.Options with
+            {
+                FullScreen = false
+            });
+        }
+
+        await InvokeAsync(StateHasChanged);
+    }
+}
---
+++ SCMM.Web.Client/Shared/Dialogs/ResponsiveDialogOptions.cs
@@ -0,0 +1,8 @@
+﻿using MudBlazor;
+
+namespace SCMM.Web.Client.Shared.Dialogs;
+
+public record ResponsiveDialogOptions : DialogOptions
+{
+    public Breakpoint FullscreenBreakpoint { get; init; } = Breakpoint.Sm;
+}
